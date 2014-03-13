dd07905bde3d1e076894a1376eae81da5b64f2a5
donaldevine@gmail.com <donaldevine@gmail.com>
2014-03-13 20:34:12 +0000

    addded totals

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
+function CartController ($scope, $location) {
+    $scope.bill = {};
 
+    $scope.items = [
+        {title: 'Widgets', quantity: 8, price: 3.95},
+        {title: 'More Stuff', quantity: 15, price: 58.88},
+        {title: 'Chips', quantity: 8, price: 2.55}
+    ];
+
+    $scope.remove = function(index){
+        $scope.items.splice(index, 1);
+    }
+
+    $scope.totalCart = function () {
+        var total = 0;
+        for (var i = 0, len = $scope.items.length; i < len; i++) {
+            total = total + $scope.items[i].price * $scope.items[i].quantity;
+        }
+
+        return total;
+    }
+
+    $scope.subTotal = function(){
+        return $scope.totalCart() - $scope.bill.discount;
+    }
+
+    function calculateDiscount(newValue, oldValue, scope)
+    {
+        scope.bill.discount = newValue > 100 ? 10 : 0;
+    }
+
+    $scope.$watch($scope.totalCart, calculateDiscount);
+
+}
